fb96436ea87a7c6d54592ec4040d59a914848183
Vi <98003224+trangpham0330@users.noreply.github.com>
2023-11-12 00:24:15 +0700

    Add files via upload

--- ultralytics/yolo/v8/detect/__init__.py
+++ ultralytics/yolo/v8/detect/__init__.py
@@ -1,9 +1,5 @@
 # Ultralytics YOLO 🚀, GPL-3.0 license
 
-__version__ = "8.0.3"
-
-from ultralytics.hub import checks
-from ultralytics.yolo.engine.model import YOLO
-from ultralytics.yolo.utils import ops
-
-__all__ = ["__version__", "YOLO", "hub", "checks"]  # allow simpler import
+from .predict import DetectionPredictor, predict
+from .train import DetectionTrainer, train
+from .val import DetectionValidator, val
